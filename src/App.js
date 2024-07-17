a5f5c898addd84cb07e406ca011eaf60edeae3c7
neerajsharmakr <neerajsharmakr047@gmail.com>
2024-07-17 18:04:37 +0530

    update the imageSearch.js

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import "./App.css";
+import ImageSearch from "./components/ImageSearch";
+import ImageEditor from "./components/ImageEditor";
+
+function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  return (
+    <div className="App">
+      <h2>Name: Neeraj Sharma</h2>
+
+      <h2>Email:neerajsharmanitc@gmail.com</h2>
+      <div className="container">
+        <h1>Image Editor</h1>
+        <ImageSearch onSelectImage={setSelectedImage} />
+        {selectedImage && <ImageEditor imageUrl={selectedImage} />}
+      </div>
+    </div>
+  );
+}
+
+export default App;
